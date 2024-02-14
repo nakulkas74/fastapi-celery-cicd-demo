5d2e33938f55c8f9a85aed9269f26089b9c07597
nakulkas74 <nakul@kascommerce.com>
2024-02-14 11:13:34 +0000

    added mongodb

--- celery_config.py
+++ celery_config.py
@@ -1,12 +1,32 @@
 import os
 from celery import Celery
 from dotenv import load_dotenv
+
+# Load environment variables from .env file
 load_dotenv('.env')
 
+# Create Celery instance
 celery_app = Celery(__name__)
+
+# Set broker connection retry on startup
+celery_app.conf.broker_connection_retry_on_startup = True
+
+# Configure Celery to use the provided broker URL
 celery_app.conf.broker_url = os.environ.get("CELERY_BROKER_URL")
+
+# Configure Celery to use MongoDB as the result backend
 celery_app.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND")
+celery_app.conf.result_backend_settings = {
+    "host": os.environ.get("MONGO_HOST", "127.0.0.1"),
+    "port": int(os.environ.get("MONGO_PORT", 27017)),
+    "database": os.environ.get("CELERY_MONGO_DB", "celery"),
+    "taskmeta_collection": os.environ.get("CELERY_MONGO_COLLECTION", "celery_taskmeta"),
+}
+
+# Set result_expires to a large value or None to disable expiration
 celery_app.conf.result_expires = os.environ.get("CELERY_RESULT_EXPIRE")
+
+# Additional Celery configuration
 celery_app.conf.include = [
     'celery_demo',
 ]
--- utils.py
+++ utils.py
@@ -1,8 +1,37 @@
 from send_mail import SendMail
-
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+load_dotenv('.env')
 sender_email = 'nakulchamariya373@gmail.com'
 sender_password = 'gnhcjjvsjznxvodv'
+
+# MongoDB configuration
+mongo_host = os.environ.get("MONGO_HOST", "127.0.0.1")
+mongo_port = int(os.environ.get("MONGO_PORT", 27017))
+mongo_db_name = os.environ.get("CELERY_MONGO_DB", "celery_jobs")
+taskmeta_collection_name = os.environ.get("CELERY_MONGO_COLLECTION", "celery_taskmeta")
+
+# Connect to MongoDB
+client = MongoClient(mongo_host, mongo_port)
+db = client[mongo_db_name]
+taskmeta_collection = db[taskmeta_collection_name]
+
+def get_celery_task_result(task_id):
+    # Query the taskmeta collection based on task_id
+    result = taskmeta_collection.find_one({"_id": task_id})
+
+    # Extract the result from the metadata
+    if result:
+        return result.get("result")
+    else:
+        return None
 
 def email_link(receiver_email_list, email_title, email_descp):
     new_mail = SendMail(receiver_email_list, email_title, email_descp, sender_email)
     new_mail.send(sender_password)
+    
+if __name__ == '__main__':
+    task_id = 'ac0f38af-695c-4d93-bb1c-9f78eaf84afc'
+    results = get_celery_task_result(task_id)
+    print(results)
